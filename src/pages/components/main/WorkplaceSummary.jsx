4f937728f07f249d1f974fe87831af15a51c2e56
MundherAl <mundher.alawi@gmail.com>
2024-02-23 00:45:41 +0100

    feat: adjust text decorations on experience summary

--- src/pages/components/main/WorkplaceSummary.jsx
+++ src/pages/components/main/WorkplaceSummary.jsx
@@ -1,9 +1,9 @@
 export default function ExperienceSummary({ title, location, role, date }) {
     return (
         <div>
-            <h3 className="text-xl font-bold">{title} - <span className="text-lg font-light">{location}</span>
+            <h3 className="text-xl font-bold">{title} // <span className="text-lg font-light">{location}</span>
             </h3>
-            <p className="text-lg">{role} - <time className="font-light">{date}</time></p>
+            <p className="text-lg">{role} // <time className="font-light">{date}</time></p>
         </div>
     );
 }
